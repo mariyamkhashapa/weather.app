ffc8c3dafa2f3783d19c82c5b38f8984f94fa8f9
mariyamkhashapa <khashapamariyam12@gmail.com>
2020-09-24 13:41:22 +0200

    first

--- app.js
+++ app.js
@@ -87,12 +87,16 @@ searchForm.addEventListener('submit', e => {
 
 
 })
-// to call the API (modern js)
+// to call the API (modern js) 
+
 const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
 const requestCity = async (city) => {
-    const URL = 'http://api.openweathermap.org/data/2.5/weather'
+   const URL = 'http://api.openweathermap.org/data/2.5/weather'
+    
+
     const query = `?q=${city}&appid=${key}`;
     const response = await fetch(URL + query);
+
     const data = await response.json();
     return data;
 
